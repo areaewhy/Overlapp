dcc3952f8e22f34ca6cd37938d520ac20abd6c54
ray.capizzo@gmail.com <ray.capizzo@gmail.com>
2023-11-06 14:26:35 -0500

    working on search-row

--- Overlapp/Components/Search/SearchRow.razor.cs
+++ Overlapp/Components/Search/SearchRow.razor.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Components;
+using Overlapp.Shared.Model;
 
 namespace Overlapp.Components
 {
 	public partial class SearchRow : ComponentBase
 	{
+		[Parameter]
+		public IMediaRecord? Media { get; set; } = null;
 
+		public string WORD { get; } = "HI";
 	}
 }
