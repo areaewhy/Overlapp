d92e808ab143451153a77fb1040d1b704e05df76
ray.capizzo@gmail.com <ray.capizzo@gmail.com>
2023-11-06 12:10:05 -0500

    adjust multi-search model

--- Overlapp.Shared/Model/Api/SearchMultiResponse.cs
+++ Overlapp.Shared/Model/Api/SearchMultiResponse.cs
@@ -1,13 +1,14 @@
 ﻿namespace Overlapp.Shared.Model
 {
 	public record SearchMultiResponse(int page, SearchMultiRecord[] results, int total_pages, int total_results);
-	public record SearchMultiRecord(bool adult, string backdrop_path, long id, string title, string original_language, string original_title, string overview, string poster_path, string media_type, int[] genre_ids, float popularity, string release_date, bool video, float vote_average, int vote_count) : IMediaRecord
+	public record SearchMultiRecord(bool adult, string backdrop_path, long id, string name, string title, string original_language, string original_title, string original_name, string overview, string poster_path, string media_type, int[] genre_ids, float popularity, string first_air_date, string release_date, bool video, float vote_average, int vote_count) : IMediaRecord
 	{
 		public long Id => id;
-		public string Title => title;
+		public string Title => IsTv ? name : title;
 		public MediaType MediaType => Enum.TryParse<MediaType>(media_type, out var t) ? t : MediaType.Unknown;
 		public string Image => poster_path;
 		public string Overview => overview;
-		public DateTime ReleaseDate => DateTime.Parse(release_date);
+		private bool IsTv => MediaType == MediaType.Tv;
+		public DateTime ReleaseDate => DateTime.Parse(IsTv ? first_air_date : release_date);
 	}
 }
